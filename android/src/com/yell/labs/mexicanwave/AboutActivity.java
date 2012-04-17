23d0d7e0b8185fe125f6ed641cb4cbfebde2c64b
Russ Anderson <russ.anderson@yellglobal.com>
2012-04-17 15:52:16 +0100

    about page now has a layout on android

--- android/src/com/yell/labs/mexicanwave/AboutActivity.java
+++ android/src/com/yell/labs/mexicanwave/AboutActivity.java
@@ -8,5 +8,6 @@ public class AboutActivity extends Activity  {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
 	}
 }
